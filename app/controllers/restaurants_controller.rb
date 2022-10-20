8ff2b59c3f67100b041a1a3507bb981320f44178
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-20 00:19:04 -0500

    enable to update a restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -18,6 +18,17 @@ class RestaurantsController < ApplicationController
     redirect_to restaurant_path(@restaurant)
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
   private
 
   def restaurant_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
   post 'restaurants', to: 'restaurants#create'
   get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  patch '/restaurants/:id', to: 'restaurants#update'
   # See all restaurants
   # See details about one restaurant
   # Create a restaurant
